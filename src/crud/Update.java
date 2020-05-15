6a89ffd5d17c09c4293a1e23261b9d8bf77d06f1
Willian <williamhernandez450@gmail.com>
2020-05-14 21:43:38 -0600

    Creo la clase Update la cual contiene el metodo para actualizar los registros

---
+++ src/crud/Update.java
@@ -0,0 +1,58 @@
+
+package crud;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author W. Hernandez
+ */
+public class Update {
+    
+ Update() throws SQLException {
+    
+Scanner  leer = new Scanner (System.in);
+student est = new student ();
+mi_conexion_CRUD batallando = new mi_conexion_CRUD ();
+
+System.out.println("<<ACTUALIZAR REGISTROS >>");
+
+System.out.println("Ingresa id del registro a modificar: ");
+est.setId_student(leer.nextInt());
+
+//REINGRESO DE DATOS PARA ACTUALIZAR 
+
+String  tabla_buscar = "tb_estudiante";
+
+String campos_buscar = "id_estudiante,  carnet_estudiante,    nom_estudiante,  ape_estudiante,  edad_estudiante";
+
+String  condicion_buscar = "id_estudiante = "+est.getId_student();
+
+batallando.desplegar_registros(tabla_buscar, campos_buscar, condicion_buscar);
+
+System.out.println("Carnet: ");
+est.setCarnet(leer.next());
+
+System.out.println("Nombre: ");
+est.setNombre(leer.next());
+
+System.out.println("Apellido: ");
+est.setApellido(leer.next());
+
+System.out.println("Edad: ");
+est.setEdad(leer.next());
+
+String tabla = "tb_estudiante";
+
+String campos_valores_nuevos = "nom_estudiante = ' "+ est.getNombre() + "',carnet_estudiante ='"+ est.getCarnet() +
+"', ape_estudiante = ' " +est.getApellido() + " ' , edad_estudiante = ' "+ est.getEdad() +"'"; //CONTINUACION DE LA FILA ANTERIOR
+
+batallando.actualizar_eliminar_registro(tabla, campos_valores_nuevos, condicion_buscar);
+System.out.println("Modificado Correctamente !!!!!!!!");
+
+
+        
+}
+}
+
