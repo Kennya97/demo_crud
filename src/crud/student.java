888ae6e3fdc45c4866a222c3c9fa9700e5d56c7e
Willian <williamhernandez450@gmail.com>
2020-05-14 21:12:49 -0600

    Agrego la clase student y sus atributos

---
+++ src/crud/student.java
@@ -0,0 +1,17 @@
+
+package crud;
+
+/**
+ *
+ * @author W. Hernandez
+ */
+public class student {
+    
+    //Atributos de la clase
+    private int id_student;
+    private String carnet;
+    private String nombre;
+    private String apellido;
+    private String edad;
+  
+}
