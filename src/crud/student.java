ca18da99bf7f06feaf27b5aa2b90dffa1ff9d46e
Willian <williamhernandez450@gmail.com>
2020-05-14 21:32:32 -0600

    Modifico la clase student agregando los metodos getters y setters

--- src/crud/student.java
+++ src/crud/student.java
@@ -13,5 +13,48 @@ public class student {
     private String nombre;
     private String apellido;
     private String edad;
+
+    //Metodos getter y setter
+    public int getId_student() {
+        return id_student;
+    }
+
+    public void setId_student(int id_student) {
+        this.id_student = id_student;
+    }
+
+    public String getCarnet() {
+        return carnet;
+    }
+
+    public void setCarnet(String carnet) {
+        this.carnet = carnet;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+    
+    
   
 }
