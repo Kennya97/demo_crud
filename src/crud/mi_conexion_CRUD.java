d11afd0275560e56dcee5d0b8716fba5fa47145e
kennWNK97 <castro21.kc@gmail.com>
2020-05-14 18:22:58 -0600

    Kennya Clarissa (SEGUNDA PARTE DE CODIGO A LA CONEXION)

--- src/crud/mi_conexion_CRUD.java
+++ src/crud/mi_conexion_CRUD.java
@@ -43,6 +43,127 @@ System.out.println("Conexion fallida !Error!  : "+e.getMessage());
 }
 }
 
+//SEGUNDA PARTE DE CODIGO AGREGADA POR KENNYA
+
+public Connection getConnection(){
+return conexion; //DEVUELVE EL OBJETO CONEXION
 }
+
+public void guardar_registros(String tabla , String campos_tabla, String valores_campos){
+//CARGAR LA CONEXION
+    
+mi_conexion_CRUD  conectar = new mi_conexion_CRUD();
+Connection cone = conectar.getConnection();
+
+try{
+//DEFINIR LA SENTENCIA SQL
+String  sqlQueryStmt = "INSERT  INTO " + tabla + "("+campos_tabla+" )  VALUES  (" +valores_campos+");";
+
+//ESTABLECEMOS LA COMUNICACION ENTRE NUESTRA APLICACION  JAVA Y LA BASE DE DATO
+Statement  stmt ; //Envia sentencia sql a la base de datos 
+
+stmt = cone.createStatement();
+
+stmt.executeUpdate(sqlQueryStmt);//Ejecutar la sentencia sql
+
+//CERRAR EL STATEMENT Y LA CONEXION SE CIERRAN EN ORDEN INVERSO DE COMO SE HAN ABIERTO 
+stmt.close();
+cone.close();
+
+System.out.println("Registro guardado correctamente!!");
+
+}catch(Exception e ){
+System.out.println(e.getMessage());
+}
+}
+
+public void actualizar_eliminar_registro(String tabla, String valores_campos_nuevos, String condicion ){
+
+//CARGAR LA CONEXION
+mi_conexion_CRUD  conectar = new mi_conexion_CRUD();
+Connection cone = conectar.getConnection();
+
+try{
+Statement stmt;
+String sqlQueryStmt;
+
+//VERIFICAR QUE VALORES CAMPOS_NUEVOS VENGA VACIA Y ASI SELECCIONAR SI ES BORRAR O ACTUALIZAR REGISTROS
+if (valores_campos_nuevos.isEmpty()){
+sqlQueryStmt = " DELETE  FROM "+ tabla + " WHERE " +  condicion + ";";
+
+}else {
+sqlQueryStmt = " UPDATE " + tabla + " SET "+valores_campos_nuevos +"  WHERE   "+condicion +";";
+}
+stmt = cone.createStatement();
+stmt.executeUpdate(sqlQueryStmt);
+stmt.close();
+cone.close();
+        
+}catch(SQLException ex){
+
+System.out.println(""+ex.getMessage());
+}
+}
+
+public void desplegar_registros (String  tabla_buscar, String campos_buscar, String condicion_buscar) throws SQLException{
+
+//CARGAR LA CONEXION
+mi_conexion_CRUD  conectar = new mi_conexion_CRUD();
+Connection cone = conectar.getConnection();
+
+//FALTA TRY
+Statement stmt;
+
+String sqlQueryStmt;
+
+if (condicion_buscar.equals("") ){
+    
+sqlQueryStmt = " SELECT " + campos_buscar+"  FROM  "+ tabla_buscar+";";
+  
+}else {
+sqlQueryStmt = " SELECT " + campos_buscar + " FROM  " + tabla_buscar + " WHERE " + condicion_buscar;
+        
+}
+stmt = cone.createStatement();
+
+stmt.executeQuery(sqlQueryStmt);
+//LE INDICAMOS QUE EJECUTE  LA CONSULTA DE LA TABLA Y LE PASAMOS POR ARGUMENTOS NUESTRAS SENTENCIAS
+
+try (ResultSet miResultSet= stmt.executeQuery(sqlQueryStmt)){
+
+if (miResultSet.next () ){//UBICA EL CURSOR EN LA PRIMERA FILA DE LA TABLA DE RESULTADO
+
+ResultSetMetaData metaData = miResultSet.getMetaData();
+int num_Columnas = metaData.getColumnCount();//OBTIENE EL NUMERO DE COLUMNAS DE LA CONSULTA
+
+System.out.println("<<REGISTROS ALMACENADOS>>");
+System.out.println("");
+
+for (int i = 1; i <= num_Columnas; i++){
+
+//MUESTRA LOS TITULOS DE LAS COLUMNAS Y %-20s\t INDICA LA SEPARACION ENTRE COLUMNAS
+System.out.printf("%-20s\t", metaData.getColumnName(i));
+
+}
+System.out.println();
+
+do{
+for (int i = 1; i<= num_Columnas; i++){
+System.out.printf("%-20s\t" , miResultSet.getObject(i));
+}
+System.out.println();
+
+}while(miResultSet.next());
+
+System.out.println();
+
+}else{
+System.out.println("No se han encontrado registros  ");
+
+}
+}
+}
+}
+
 
 
